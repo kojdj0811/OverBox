6f9fe57c57971f8425f91aa8a4007cecfb4b0690
mooseong <hello.git.antjd0008@naver.com>
2022-08-27 11:08:51 +0900

    저장소 기능 구현

--- Client/Assets/Moru/Scripts/Obstacle/Storage.cs
+++ Client/Assets/Moru/Scripts/Obstacle/Storage.cs
@@ -22,26 +22,36 @@ namespace Moru
             } 
         }
 
+        //테스트 전용 변수입니다.
+        public bool TestToggle = false;
+
+
+
         /// <summary>
         /// 아이템을 하나 꺼냅니다.
         /// </summary>
-        public void PullItem()
+        private void PullItem()
         {
             //플레이어가 아이템을 아무것도 들고 있지 않다면
             if(true)
             {
                 //상품이 충분하다면
                 if(StorageBox.TryProductPull(out int result))
-                { 
+                {
                     //플레이어가 해당물건을 들고 있는 상태로 만듭니다.
+                    Debug.Log($"플레이어가 {TargetProduct}를 {result}만큼 꺼냈습니다.");
                 }
             }
+
+            //테스트 전용 코드입니다.
+            TestToggle = !TestToggle;
+            //테스트 전용 코드입니다.
         }
 
         /// <summary>
         /// 아이템을 하나 세이브합니다.
         /// </summary>
-        public void TrySaveItem()
+        private void TrySaveItem()
         {
             //플레이어가 들고 있는 아이템이 보관함이 보관할 수 있는 종류와 같은 것이라면
             if (true)
@@ -49,7 +59,13 @@ namespace Moru
                 //상품을 보관함에 저장합니다.
                 StorageBox.ProductKeep();
                 //플레이어의 아이템이 비어있는 상태로 만듭니다.
+                Debug.Log($"플레이어가 {TargetProduct}를 집어넣었습니다.");
             }
+
+
+            //테스트 전용 코드입니다.
+            TestToggle = !TestToggle;
+            //테스트 전용 코드입니다.
         }
 
         private void OnCollisionStay(Collision collision)
@@ -68,8 +84,19 @@ namespace Moru
 
         public override void OnInteractive(Hyerin.Player pl)
         {
+            //일단 테스트용입니다.
             //플레이어가 아이템 들고 있는지 판별
-            //플레이어가 아이템을 들고 있다면 어떤 아이템을 들고 있는지 판별
+            if(TestToggle)
+            {
+                //플레이어가 아이템을 들고 있다면 어떤 아이템을 들고 있는지 판별
+                TrySaveItem();
+            }
+            //플레이어가 아이템을 들고 있지 않다면
+            else if(!TestToggle)
+            {
+                PullItem();
+            }
+
         }
 
         #endregion
