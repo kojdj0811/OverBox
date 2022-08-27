a10e7df722228aa3e378a513d571810288542f06
hyerinlee <herring027@gmail.com>
2022-08-27 13:31:15 +0900

    플레이어 물건 들기/놓기

--- Client/Assets/Hyerin/Scripts/Player.cs
+++ Client/Assets/Hyerin/Scripts/Player.cs
@@ -6,6 +6,14 @@ namespace Hyerin
 {
     public class Player : SingleToneMono<Player>
     {
+        #region Const
+        public const string Box = "Box";
+        public const string Computer = "Computer";
+        public const string ConveyorBelt = "ConveyorBelt";
+        public const string Ground = "Ground";
+        public const string Storage = "Storage";
+        #endregion
+
         /// <summary>
         /// 플레이어의 상태 인덱스입니다.
         /// </summary>
@@ -18,13 +26,11 @@ namespace Hyerin
         [Range(0f, 1f)]
         public float keyPressCooldownMax = 0.1f;
 
-
-        public bool isCarrying;
-        public int carryingObj; // 0~6:물건, 7:박스, 8:없음
+        public int carryingIndex; // 0~6:물건, 7:박스, 8:없음
         public State state;
-        //public Moru.MoruDefine.Product carryingProduct;
+        public GameObject carryingObj;
 
-        private float keyPressCooldown;
+        private bool isSpacebarPressed;
         // 발주 상품 목록
         private int[] items;
         private Animator anim;
@@ -33,17 +39,16 @@ namespace Hyerin
 
         void Start()
         {
-            isCarrying = false;
-            carryingObj = 8;
+            carryingIndex = 8;
             state = State.Movable;
-            keyPressCooldown = 0f;
+            isSpacebarPressed = false;
             anim = GetComponent<Animator>();
             rb = GetComponent<Rigidbody>();
         }
 
         void OnDrawGizmos()
         {
-            int layerMask = (1 << LayerMask.GetMask("Ground"));
+            int layerMask = (1 << LayerMask.GetMask(Ground));
             RaycastHit hit;
             bool isHit = Physics.BoxCast(transform.position, transform.lossyScale / 2, transform.forward, out hit, transform.rotation, boxCastMaxDistance, layerMask);
 
@@ -84,12 +89,14 @@ namespace Hyerin
         /// </summary>
         void Update()
         {
-            if (keyPressCooldown > 0f) keyPressCooldown -= Time.deltaTime;
+            isSpacebarPressed = false;
 
-            int layerMask = (1 << LayerMask.GetMask("Ground"));
+            int layerMask = (1 << LayerMask.GetMask(Ground));
             RaycastHit hit;
             // 플레이어 주변에 상호작용 가능한 오브젝트가 있는지 확인합니다.(레이저를 발사할 위치, 사각형의 각 좌표의 절판 크기, 발사 방향, 충돌 결과, 회전 각도, 최대 거리)
             bool isHit = Physics.BoxCast(transform.position, transform.lossyScale / 2, transform.forward, out hit, transform.rotation, boxCastMaxDistance);
+
+            if (Input.GetKeyUp(KeyCode.Space)) isSpacebarPressed = false;
 
             // 스페이스바를 눌렀을 때 플레이어 상태에 따라 상호작용합니다.
             if (Input.GetKeyDown(KeyCode.Space))
@@ -102,20 +109,20 @@ namespace Hyerin
 
                     // 플레이어의 상태를 변경합니다.
                     string hitColName = hit.collider.name;
-                    Debug.Log(hitColName);
                     switch (hit.collider.name)
                     {
-                        case "Box":
+                        case Box:
+                        case Storage:
+                            Carry(hit.collider.gameObject);
+                            isSpacebarPressed = true;
                             break;
-                        case "Computer":
+                        case Computer:
                             Debug.Log("컴퓨터 사용");
                             state = State.Computer;
-                            keyPressCooldown = keyPressCooldownMax;
+                            isSpacebarPressed = true;
                             break;
-                        case "ConveyorBelt":
+                        case ConveyorBelt:
                             state = State.Audition;
-                            break;
-                        case "Storage":
                             break;
                         default:
                             break;
@@ -123,11 +130,14 @@ namespace Hyerin
                 }
 
                 // 컴퓨터 사용 상태라면 발주 주문을 확정합니다.
-                if (state == State.Computer && keyPressCooldown<=0)
+                if (state == State.Computer && !isSpacebarPressed)
                 {
                     Debug.Log("발주됨");
                     //Moru.MoruDefine.delegate_Delivery?.invoke(items);
                 }
+
+                // 물건을 들고 있었다면 내려놓습니다.
+                if (carryingObj && !isSpacebarPressed) Lay();
             }
 
             if ( Input.GetKeyDown(KeyCode.Q))
@@ -169,6 +179,36 @@ namespace Hyerin
                         break;
                 }
             }
+        }
+
+        public bool IsCarrying()
+        {
+            return carryingObj != null;
+        }
+
+        /// <summary>
+        /// 물건을 듭니다.
+        /// </summary>
+        private void Carry(GameObject obj)
+        {
+            if (carryingObj) return;
+
+            Debug.Log(obj.name + "을 들었음");
+            obj.transform.SetParent(transform);
+            carryingObj = obj;
+            if (obj.name == Box) carryingIndex = 7;
+            else carryingIndex = (int)obj.GetComponent<Moru.Storage>().TargetProduct;
+        }
+        
+        /// <summary>
+        /// 물건을 내려놓습니다.
+        /// </summary>
+        private void Lay()
+        {
+            Debug.Log(carryingObj.name + "을 놓음");
+            carryingObj.transform.SetParent(null);
+            carryingObj = null;
+            carryingIndex = 8;
         }
     }
 }
